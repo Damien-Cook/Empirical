720e5af1259f4f3ce1f1305fbb487f9ff3130167
RealCookieOG <87521446+RealCookieOG@users.noreply.github.com>
2022-01-24 15:04:58 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -98,3 +98,26 @@ $(document).ready(function(){
         .addTo(controller);
   }
 });
+
+
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
